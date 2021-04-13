b26859093e24e4310baaa1416cf4ea41c2776291
ChristianCudby <christiancudby@hotmail.com>
2021-04-13 18:00:17 -0400

    commit tree 2a8c0135b5cc68215de764bd9e325a5f96bc4ce0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const names = ["Lisa", "Kaitlin", "Jan"];
+let cards = []
+
+function writeCards() {
+    let event = "surprise";
+    for (let i = 0; i < names.length; i++) {
+        cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return cards;
+}
+
+function countDown() {
+    let count = 10;
+    while (count > -1) {
+        console.log(count--);
+    }
+}
